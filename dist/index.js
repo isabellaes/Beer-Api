37c6c98ef76c95cdd95952e29032dee35ab07629
isabellaes <borjessonisabella@gmail.com>
2023-12-19 16:02:27 +0100

    paginatin

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-function getBeer() {
+function getBeers() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const response = yield fetch("https://api.punkapi.com/v2/beers");
@@ -27,15 +27,90 @@ function getBeer() {
         }
     });
 }
+getBeers();
 const arrayOfBeers = [];
-getBeer();
+console.log(arrayOfBeers);
+const card = document.getElementById("card");
+const div = document.getElementById("beer-info");
 const randomButton = document.querySelector("#random-beer");
 randomButton.addEventListener("click", () => {
-    const card = document.getElementById("card");
+    const randomObject = getRandomBeer();
+    showCard(randomObject);
+});
+document.querySelector("#search").addEventListener("click", (e) => {
+    e.preventDefault();
+    /*  const result = searchBeer();
+    if (result) {
+      searchResultList(result);
+    } */
+    searchResultList(arrayOfBeers);
+});
+function showCard(randomObject) {
+    if (card.classList.contains("hide")) {
+        card.classList.toggle("hide");
+    }
     const h1 = card.querySelector("h1");
     const button = card.querySelector("button");
     const img = card.querySelector("img");
-    const randomObject = arrayOfBeers[Math.floor(Math.random() * arrayOfBeers.length)];
     h1.innerHTML = randomObject.name;
     img.src = randomObject.image_url;
-});
+    button.addEventListener("click", () => {
+        showDetails(randomObject);
+    });
+}
+function getRandomBeer() {
+    return arrayOfBeers[Math.floor(Math.random() * arrayOfBeers.length)];
+}
+function showDetails(beer) {
+    if (card.classList.contains("hide") != true) {
+        card.classList.toggle("hide");
+    }
+    randomButton.classList.toggle("hide");
+    div.classList.toggle("hide");
+    //lägg till info i detaljsidan
+    document.getElementById("name").innerHTML = beer.name;
+    document.getElementById("description").innerHTML = beer.description;
+    document.getElementById("abv").innerHTML = beer.abv.toString();
+    document.getElementById("volume").innerHTML =
+        beer.volume.value + " " + beer.volume.unit;
+    div.querySelector("img").src = beer.image_url;
+    document.getElementById("food-pairing").innerHTML =
+        beer.food_pairing.toString();
+    document.getElementById("brewers-tips").innerHTML = beer.brewers_tips;
+    div.querySelector("button").addEventListener("click", () => {
+        randomButton.classList.toggle("hide");
+        div.classList.toggle("hide");
+    });
+}
+function searchBeer() {
+    const searchValue = document.getElementById("search-name");
+    return arrayOfBeers.filter((beer) => beer.name.toLocaleLowerCase() === searchValue.value);
+}
+function searchResultList(beers) {
+    const div = document.getElementById("search-result");
+    const li = div.querySelectorAll("li");
+    const pagination = document.getElementById("pagination");
+    const number = Math.ceil(beers.length / 10);
+    console.log(number);
+    let indexBeer = 0;
+    for (let index1 = 0; index1 <= number - 1; index1++) {
+        const element = document.createElement("a");
+        element.innerHTML = (index1 + 1).toString();
+        console.log(index1);
+        element.addEventListener("click", () => {
+            for (let index = 0; index < 10; index++) {
+                if (beers[indexBeer]) {
+                    li[index].innerHTML = beers[indexBeer].name;
+                    li[index].addEventListener("click", () => {
+                        showDetails(beers[index]);
+                    });
+                    indexBeer++;
+                }
+                else {
+                    li[index].innerHTML = "";
+                }
+            }
+        });
+        pagination.appendChild(element);
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -7,33 +7,31 @@ interface beer {
     value: number;
     unit: string;
   };
-  ingredients: [
-    {
-      hops: [
-        {
-          name: string;
-          add: string;
-          attribut: string;
-          amount: { value: number; unit: string };
-        }
-      ];
-      malt: [
-        {
-          name: string;
-          amount: {
-            value: number;
-            unit: string;
-          };
-        }
-      ];
-      yeast: string;
-    }
-  ];
+  ingredients: {
+    hops: [
+      {
+        name: string;
+        add: string;
+        attribut: string;
+        amount: { value: number; unit: string };
+      }
+    ];
+    malt: [
+      {
+        name: string;
+        amount: {
+          value: number;
+          unit: string;
+        };
+      }
+    ];
+    yeast: string;
+  };
   food_pairing: string[];
   brewers_tips: string;
 }
 
-async function getBeer(): Promise<beer[]> {
+async function getBeers(): Promise<beer[]> {
   try {
     const response = await fetch("https://api.punkapi.com/v2/beers");
     console.log(response);
@@ -51,18 +49,109 @@ async function getBeer(): Promise<beer[]> {
   }
 }
 
+getBeers();
+
 const arrayOfBeers: beer[] = [];
-getBeer();
+console.log(arrayOfBeers);
+
+const card = document.getElementById("card");
+const div = document.getElementById("beer-info");
 
 const randomButton = document.querySelector("#random-beer");
 randomButton.addEventListener("click", () => {
-  const card = document.getElementById("card");
+  const randomObject: beer = getRandomBeer();
+  showCard(randomObject);
+});
+
+document.querySelector("#search").addEventListener("click", (e) => {
+  e.preventDefault();
+  /*  const result = searchBeer();
+  if (result) {
+    searchResultList(result);
+  } */
+  searchResultList(arrayOfBeers);
+});
+
+function showCard(randomObject: beer) {
+  if (card.classList.contains("hide")) {
+    card.classList.toggle("hide");
+  }
+
   const h1 = card.querySelector("h1");
   const button = card.querySelector("button");
   const img = card.querySelector("img");
-
-  const randomObject =
-    arrayOfBeers[Math.floor(Math.random() * arrayOfBeers.length)];
   h1.innerHTML = randomObject.name;
   img.src = randomObject.image_url;
-});
+  button.addEventListener("click", () => {
+    showDetails(randomObject);
+  });
+}
+
+function getRandomBeer(): beer {
+  return arrayOfBeers[Math.floor(Math.random() * arrayOfBeers.length)];
+}
+
+function showDetails(beer: beer) {
+  if (card.classList.contains("hide") != true) {
+    card.classList.toggle("hide");
+  }
+
+  randomButton.classList.toggle("hide");
+  div.classList.toggle("hide");
+  //lägg till info i detaljsidan
+  document.getElementById("name").innerHTML = beer.name;
+  document.getElementById("description").innerHTML = beer.description;
+  document.getElementById("abv").innerHTML = beer.abv.toString();
+  document.getElementById("volume").innerHTML =
+    beer.volume.value + " " + beer.volume.unit;
+  div.querySelector("img").src = beer.image_url;
+  document.getElementById("food-pairing").innerHTML =
+    beer.food_pairing.toString();
+  document.getElementById("brewers-tips").innerHTML = beer.brewers_tips;
+  div.querySelector("button").addEventListener("click", () => {
+    randomButton.classList.toggle("hide");
+    div.classList.toggle("hide");
+  });
+}
+
+function searchBeer(): beer[] | void {
+  const searchValue = document.getElementById(
+    "search-name"
+  ) as HTMLInputElement;
+  return arrayOfBeers.filter(
+    (beer) => beer.name.toLocaleLowerCase() === searchValue.value
+  );
+}
+
+function searchResultList(beers: beer[]): void {
+  const div = document.getElementById("search-result");
+  const li = div.querySelectorAll("li");
+
+  const pagination = document.getElementById("pagination");
+
+  const number = Math.ceil(beers.length / 10);
+  console.log(number);
+
+  let indexBeer = 0;
+  for (let index1 = 0; index1 <= number - 1; index1++) {
+    const element = document.createElement("a");
+
+    element.innerHTML = (index1 + 1).toString();
+    console.log(index1);
+
+    element.addEventListener("click", () => {
+      for (let index = 0; index < 10; index++) {
+        if (beers[indexBeer]) {
+          li[index].innerHTML = beers[indexBeer].name;
+          li[index].addEventListener("click", () => {
+            showDetails(beers[index]);
+          });
+          indexBeer++;
+        } else {
+          li[index].innerHTML = "";
+        }
+      }
+    });
+    pagination.appendChild(element);
+  }
+}
